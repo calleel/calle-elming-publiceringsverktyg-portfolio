3abf2c1f010e8182dd91fdb24a95ca4dba3fa89d
calleel <55479446+calleel@users.noreply.github.com>
2020-10-14 22:30:16 +0200

    Changed styles for nav, hero, jobs, color scheme

--- student_frontend/src/components/Hero.js
+++ student_frontend/src/components/Hero.js
@@ -2,7 +2,6 @@ import React from "react"
 import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
-import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
 const query = graphql`
@@ -29,32 +28,37 @@ const Hero = () => {
   return (
     <header className="hero">
       <div className="section-center hero-center">
-        <article className="hero-info">
-          <div>
-            <h1>Calle Elming</h1>
-            <div className="underline"></div>
-            <h4>Blivande systemutvecklare</h4>
-            <p>Studerar just nu till Systemutvecklare och är 
-            <br />
-            alltid på jakt för att lära mig nya saker.</p>
-            <p>
-            Jag har erfarenhet av bland annat:
-            <br />
-            - HTML, CSS, Javascript
-            <br />
-            - Gatsby
-            <br />
-            - Strapi
-            <br />
-            - C# .NET, Blazor, MVC
-            </p>
-            
-            <Link to="/contact" className="btn">
-              kontakta mig
-            </Link>
+        <div class="hero-content-container">
+          <article className="hero-info">
+            <div>
+              <h1>Calle Elming</h1>
+              <div className="underline"></div>
+              <h4>Blivande systemutvecklare</h4>
+              <p>
+                Studerar just nu till Systemutvecklare och är
+                <br />
+                alltid på jakt för att lära mig nya saker.
+              </p>
+              <p>
+                Jag har erfarenhet av bland annat:
+                <br />
+                - HTML, CSS, Javascript
+                <br />
+                - Gatsby
+                <br />
+                - Strapi
+                <br />- C# .NET, Blazor, MVC
+              </p>
+
+              <Link to="/contact" className="btn">
+                kontakta mig
+              </Link>
+            </div>
+          </article>
+          <div class="image-container">
+            <Image fluid={fluid} className="hero-img" />
           </div>
-        </article>
-        <Image fluid={fluid} className="hero-img" />
+        </div>
       </div>
     </header>
   )
